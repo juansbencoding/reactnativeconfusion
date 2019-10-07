94a4a7a6c9a636e0e48720ad39842cb528d9f682
Juan Salgado <juans.bencoding@gmail.com>
2019-10-07 05:48:21 -0700

    Secure Storage

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent'
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
@@ -139,6 +140,24 @@ const FavoritesNavigator = createStackNavigator({
     })
 });
 
+const LoginNavigator = createStackNavigator({
+    Login: { screen: Login }
+}, { 
+    navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTitleStyle: {
+            color: '#fff'
+        },
+        headerTintColor: '#fff', 
+        headerLeft: <Icon name='menu' size={24}
+            color='white' 
+            onPress={() => navigation.toggleDrawer()}
+            />
+    })
+});
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView style={styles.container}
@@ -159,6 +178,23 @@ const CustomDrawerContentComponent = (props) => (
 
 
 const MainNavigator = createDrawerNavigator({
+
+    Login: {
+        screen: LoginNavigator,
+        navigationOptions: {
+            title: 'Login', 
+            drawerLabel: 'Login',
+            drawerIcon: ({ tintColor }) => (
+                <Icon
+                    name='sign-in'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                    />
+            )
+        }
+    }, 
+
     Home: {
         screen: HomeNavigator,
         navigationOptions: {
@@ -250,6 +286,7 @@ const MainNavigator = createDrawerNavigator({
         }
     }
 }, {
+    initialRouteName: 'Home', 
     drawerBackgroundColor: '#D1C4E9',
     contentComponent: CustomDrawerContentComponent
 });
